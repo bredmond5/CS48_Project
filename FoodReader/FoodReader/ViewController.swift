7bd17abfa1fe71d94e6f1ca6b4ef63b3daefeecb
bredmond5 <briceredmond@gmail.com>
2019-04-12 14:47:19 -0700

    added the xcode project

---
+++ FoodReader/FoodReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodReader
+//
+//  Created by Brice Redmond on 4/12/19.
+//  Copyright © 2019 CS48. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
