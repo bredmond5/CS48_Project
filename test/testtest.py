3b8eb6ff8bf9099f14e1c9b64f740f6bf2b5a135
jtlee1 <38050128+jtlee1@users.noreply.github.com>
2019-04-17 17:16:54 -0700

    Create testtest.py

---
+++ test/testtest.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)),'python'))
+
+from test import helloworld
+
+import pytest
+
+class TestCalculator(object):
+
+    def setup_method(self):
+        self.h = helloworls()
+
+    def teardown_method(self):
+        self.h = None
+
+    def test_hello():
+
+        res = self.h.hello()
+        assert res == "Hello World"
