40c395037263f19de75b820a76ff8e8a0fb57568
Justin Lee <jtlee@ucsb.edu>
2019-04-24 11:21:58 -0700

    barscanner has bug but firebase uploaded

--- MyPricePal/MyPricePal/AppDelegate.swift
+++ MyPricePal/MyPricePal/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -23,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let mainVC = MainViewController()
         window?.rootViewController = UINavigationController(rootViewController: mainVC)
+        
+        FirebaseApp.configure()
         
         return true
     }
