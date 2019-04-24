d8d8920dec75022a2308e434324ba060739415fd
Justin Lee <jtlee@ucsb.edu>
2019-04-23 23:47:36 -0700

    link firebase

--- MyPricePal/MyPricePal/AppDelegate.swift
+++ MyPricePal/MyPricePal/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -22,6 +23,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let mainVC = MainViewController()
         window?.rootViewController = UINavigationController(rootViewController: mainVC)
+        
+        FirebaseApp.configure()
         
         return true
     }
