3e254e8c4fda65c5aadd02e68ba8cc447d8419c7
Ramon Verdin <ramonverdin@umail.ucsb.edu>
2019-05-17 20:46:52 -0700

    Fixed searching error

--- MyPricePal/MyPricePal/PriceFinder.swift
+++ MyPricePal/MyPricePal/PriceFinder.swift
@@ -38,7 +38,7 @@ class PriceFinder: NSObject {
     }
     
     struct Content: Decodable{
-        let url: String
+        let url: String? = ""
         let offers: [Offers]
     }
     
@@ -58,42 +58,38 @@ class PriceFinder: NSObject {
         initialRequest.httpBody = fields.data(using: String.Encoding.utf8)
         initialRequest.httpMethod = "POST"
         let initialTask = initialSession.dataTask(with: initialRequest as URLRequest) { (data, response, error) in
-            if let response = response{
-                print(response)
-            }
-            
             guard let data = data else {return}
             do{
                 let requestJSON = try JSONDecoder().decode(initialRequestJSON.self, from: data) //make json parsing easy
-                self.checkStatus(requestJSON.job_id, baseUrl: base) //once the request is sent, gets job_id from JSON and passes it to another function
+                self.checkStatus(requestJSON.job_id, baseUrl: base, barcode: barcode) //once the request is sent, gets job_id from JSON and passes it to another function
             }catch{
-                print("Error")
+                print("Error getBestPrice")
             }
         }
         initialTask.resume()
         
     }
     //the function that I'm using to check if the job is done running and ready to return
-    func checkStatus(_ id: String, baseUrl: String){ //must run this to make sure the job has finished running in order to get the data
+    func checkStatus(_ id: String, baseUrl: String, barcode: String){ //must run this to make sure the job has finished running in order to get the data
+        var count = 0
         let token = "?token=IZSVNVFBCNHRBVHLOVVECMFFEYEFVFWQKSXZOVWIYYZOLEUJUQZPEKVWIJHXFPMB"
         let statusURL = URL(string: baseUrl + id + token)
         let statusSession = URLSession.shared
         let statusTask = statusSession.dataTask(with: statusURL!) { (data, response, error) in
-            if let response = response{
-                print(response)
-            }
             if let data = data{
-                print(data)
-                
                 do{
                     let checkJSON = try JSONDecoder().decode(checkingRequest.self, from: data)
                     print(checkJSON.status)
                     if(checkJSON.status != "finished"){
-                        self.checkStatus(id, baseUrl: baseUrl)
+                        self.checkStatus(id, baseUrl: baseUrl, barcode: barcode)
+                    }else{
+                        count = 1
                     }
-                    self.getJSON(id, baseUrl: baseUrl)
+                    if(count == 1){
+                        self.getJSON(id, baseUrl: baseUrl, barcode: barcode)
+                    }
                 }catch{
-                    print(error)
+                    print("Error checkStatus")
                 }
             }
         }
@@ -101,32 +97,31 @@ class PriceFinder: NSObject {
     }
     
     //this function gets the JSON with the data
-    func getJSON(_ id: String, baseUrl: String){
+    func getJSON(_ id: String, baseUrl: String, barcode: String){
+        print(barcode)
         let token = "?token=IZSVNVFBCNHRBVHLOVVECMFFEYEFVFWQKSXZOVWIYYZOLEUJUQZPEKVWIJHXFPMB"
         let downloadURL = URL(string: baseUrl + id + "/download.json" + token)
         let jsonSession = URLSession.shared
         let jsonTask = jsonSession.dataTask(with: downloadURL!) { (data, response, error) in
-            if let response = response{
-                print(response)
-            }
-            print(id)
-            if let data = data{
-                do {
-                    let responseJSON = try JSONDecoder().decode(returnJSON.self, from: data)
-                    self.prices.append(responseJSON.results[0].content.url)
-                    for i in (responseJSON.results[0].content.offers).indices{
-                      self.prices.append(responseJSON.results[0].content.offers[i].shop_name)
-
-                        self.prices.append(responseJSON.results[0].content.offers[i].price)
-                        self.prices.append(responseJSON.results[0].content.offers[i].url)
-                    }
-                    self.priceDelegate?.returnPrices(self.prices)
-
-                }catch{
-                    print(error)
+            guard let data = data else {return}
+            print(data)
+            do {
+                let responseJSON = try JSONDecoder().decode(returnJSON.self, from: data)
+                if((responseJSON.results[0].content.url!) != ""){
+                    self.prices.append(responseJSON.results[0].content.url!)
+                }else{
+                    self.prices.append("https://www.google.com/search?tbm=shop&hl=en&source=hp&biw=&bih=&q=" + barcode + "&oq=" + barcode + "&gs_l=products-cc.3...1173.1173.0.1942.1.1.0.0.0.0.7.7.1.1.0....0...1ac.2.34.products-cc..1.0.0.az1Q1kQyBq8")
                 }
+                for i in (responseJSON.results[0].content.offers).indices{
+                    self.prices.append(responseJSON.results[0].content.offers[i].shop_name)
+                    self.prices.append(responseJSON.results[0].content.offers[i].price)
+                    self.prices.append(responseJSON.results[0].content.offers[i].url)
+                }
+                self.priceDelegate?.returnPrices(self.prices)
+                
+            }catch{
+                print("Error getJSON")
             }
-            
         }
         jsonTask.resume()
     }
