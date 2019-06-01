fa77792e4b058357e8514e7e5ba9b535019dcde0
Justin Lee <jtlee@ucsb.edu>
2019-05-31 20:17:06 -0700

    fix test

--- MyPricePal/MyPricePalTests/MyPricePalTests.swift
+++ MyPricePal/MyPricePalTests/MyPricePalTests.swift
@@ -10,12 +10,12 @@ import XCTest
 @testable import MyPricePal
 class MyPricePalTests: XCTestCase {
     
-  //  var mainVC: MainViewController?
-  //  var searchVC: SearchViewController?
+  var mainVC: MainViewController?
+  var searchVC: SearchViewController?
     
     override func setUp() {
-       // mainVC = MainViewController()
-       // searchVC = SearchViewController()
+       mainVC = MainViewController()
+       searchVC = SearchViewController()
     }
     
     override func tearDown() {
@@ -29,45 +29,44 @@ class MyPricePalTests: XCTestCase {
         }
     }
     
- /*   func testAddItem(){
-        searchVC?.giveItemScanned("Carmex Lip Balm", barcodeString: "083078113131", keywordString: <#[String]#>)
+    func testAddItem(){
+        searchVC?.giveItemScanned(itemN: "Carmex Lip Balm", barcodeString: "083078113131", keywordString: ["Lip"], priceArray: ["10"])
         XCTAssert(searchVC?.returnNumItems() == 1, "SearchVC does not have the correct amount of items")
     }
     
-    func testAddMaxItems() {
+   func testAddMaxItems() {
         
         for i in 0...searchVC!.maxItems {
-            searchVC?.giveItemScanned("Deodorant \(i)")
+            searchVC?.giveItemScanned(itemN: "Deodorant", barcodeString: "083078113131", keywordString: ["Deodorant"], priceArray: ["10"])
         }
         
         XCTAssert(searchVC?.returnNumItems() == searchVC?.maxItems, "SearchVC added \(String(describing: searchVC?.returnNumItems())) items")
-        XCTAssert(searchVC!.items[0] == "Deodorant \(searchVC!.maxItems)", "\(searchVC!.items[0]) != Deodorant \(searchVC!.maxItems)")
-        XCTAssert(searchVC?.items[(searchVC?.maxItems)! - 1] == "Deodorant \(1)", "not Deodorant 1")
+        XCTAssert(searchVC!.items[0].itemN == "Deodorant \(searchVC!.maxItems)", "\(searchVC!.items[0]) != Deodorant \(searchVC!.maxItems)")
+        XCTAssert(searchVC?.items[(searchVC?.maxItems)! - 1].itemN == "Deodorant \(1)", "not Deodorant 1")
     }
     
     func testShiftItems() {
-        searchVC?.giveItemScanned("Deodorant")
-        searchVC?.giveItemScanned("Gum")
-        searchVC?.giveItemScanned("Binder")
-        searchVC?.giveItemScanned("Deodorant")
+        searchVC?.giveItemScanned(itemN: "Deodorant", barcodeString: "083078113131", keywordString: ["Deodorant"], priceArray: ["10"])
+        searchVC?.giveItemScanned(itemN: "Carmex Lip Balm", barcodeString: "083078113131", keywordString: ["Lip"], priceArray: ["10"])
+        searchVC?.giveItemScanned(itemN: "popcorn", barcodeString: "083078113131", keywordString: ["Lip"], priceArray: ["10"])
+        searchVC?.giveItemScanned(itemN: "Deodorant", barcodeString: "083078113131", keywordString: ["Deodorant"], priceArray: ["10"])
         
-        XCTAssert(searchVC?.items[0] == "Deodorant", "Deodorant != \(searchVC!.items[0])")
-        XCTAssert(searchVC?.items[1] == "Binder", "Binder != \(searchVC!.items[1])")
-        XCTAssert(searchVC?.items[2] == "Gum", "Gum != \(searchVC!.items[2])")
+        XCTAssert(searchVC?.items[0].itemN == "Deodorant", "Deodorant != \(searchVC!.items[0])")
+        XCTAssert(searchVC?.items[1].itemN == "Carmex Lip Balm", "Carmex Lip Balm != \(searchVC!.items[1])")
+        XCTAssert(searchVC?.items[2].itemN == "popcorn", "popcorn != \(searchVC!.items[2])")
     }
-    
     func testDeleteCell() {
-        searchVC?.giveItemScanned("Deodorant")
-        searchVC?.giveItemScanned("Gum")
-        searchVC?.giveItemScanned("Binder")
+        searchVC?.giveItemScanned(itemN: "Deodorant", barcodeString: "083078113131", keywordString: ["Deodorant"], priceArray: ["10"])
+        searchVC?.giveItemScanned(itemN: "Carmex Lip Balm", barcodeString: "083078113131", keywordString: ["Lip"], priceArray: ["10"])
+        searchVC?.giveItemScanned(itemN: "popcorn", barcodeString: "083078113131", keywordString: ["Lip"], priceArray: ["10"])
         let tableView = searchVC?.tableView
         let deletionIndexPath = IndexPath(item: 0, section: 0)
         let cell = tableView?.cellForRow(at: deletionIndexPath)
         searchVC?.deleteCell(cell: cell!)
         
-        XCTAssert(searchVC?.items[0] == "Gum", "items[0] = \(String(describing: searchVC?.items[0]))")
-        XCTAssert(searchVC?.items[1] == "Deodorant", "items[1] = \(String(describing: searchVC?.items[1]))")
-    }*/
+        XCTAssert(searchVC?.items[0].itemN == "Carmex Lip Balm", "items[0] = \(String(describing: searchVC?.items[0]))")
+        XCTAssert(searchVC?.items[1].itemN == "Deodorant", "items[1] = \(String(describing: searchVC?.items[1]))")
+    }
     
     func testLoadViewItemVC() {
         let itemVC = ItemViewController()
